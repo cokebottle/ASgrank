61cbeea97fd21ade31f7a5f1dfdde13da0ba9bdf
cokebottle <cok3bottle@gmail.com>
2014-02-27 15:13:59 +0200

    add the main files

---
+++ asnum.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+"""
+A tool for quering RIPE api obtain AS peer information, query CIRCL BGPrank and obtain reputation rank
+
+"""
+__author__= 'Cosmin Ciobanu, Romain Bourgue, Razvan Gavrila'
+__license__= 'GPL'
+__contact__= 'cosmin.ciobanu@gmail.com'
+__version__ = 1.0
+
+#import modules
+import pymongo,json, time
+import argparse
+import sys
+import socket
+import urllib2
+
+
+#setup database connection 
+connect = pymongo.MongoClient("localhost", 27017)
+db = connect.asnum
+#select collection from MongoDB
+EU_memberstates = ["AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "ES", "FI", "FR", "GB", "GR", "HU", "IE", "IT", "LT", "LU", "LV", "MT", "NL", "PL", "PT", "RO", "SE", "SI", "SK"]
+asnlist = {}
+
+
+class Asnode:
+   def jsonable(self):
+      return self.__dict__
+   pass
+
+
+#function to query RIPE api for country AS information
+def countryQueryRipe(country = None):
+    print "Quering RIPE API for Country"+ country + "\n"
+    response = json.load(urllib2.urlopen('http://stat.ripe.net/data/country-resource-list/data.json?resource='+ country +'&resources=as'))
+    #countryJson = json.dumps(response, sort_keys=True, indent=4, separators=(',', ': ')) #print formatted json
+    results = response["data"]["resources"]["asn"]
+    res=[]
+    for item in results:#fix me later
+        res.append(int(item))
+    return res #[132,4654,6546,3513]
+    #db.country.insert(response)
+    
+def peerQueryRipe(asnumber = None):
+    print "Quering RIPE API for AS"+ str(asnumber) + " peers\n"
+    response = json.load(urllib2.urlopen('http://stat.ripe.net/data/asn-neighbours/data.json?resource=AS'+str(asnumber)))
+    peerlist=[]
+    for neighbour in response["data"]["neighbours"]:
+        peerlist.append(neighbour["asn"])
+    return peerlist
+    #print response["data"]["neighbours"][0]["asn"]
+    #print json.dumps(response, sort_keys=True, indent=4, separators=(',', ': ')) #print formatted json
+
+#function to query BGPrank api
+def queryBgprank(query = None):
+    HOST = 'pdns.circl.lu'    # The remote services host
+    PORT = 43                 # The same port as used by the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    #print query
+    s.send(str(query) + "\r\n") #Send ASNumber and query for reputation information
+    data = s.recv(1024)
+    s.close()
+    data = data.split("\n") #Split by new-line into array and print second line
+    
+    return data[1].split(",")[1]
+#    print 'Received', repr(data)
+
+#function to query TeamCymru IP2ASN
+def queryTeamCymru(query = None):
+    HOST = 'whois.cymru.com'    # The remote services host
+    PORT = 43                 # The same port as used by the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    #print query
+    s.send(" -v" + query + "\r\n") #Send ASNumber and query for reputation information
+    data = s.recv(1024)
+    s.close()
+    data = data.split("\n") #Split by new-line into array and print second line
+
+#next step geolocation plotting: https://stat.ripe.net/data/geoloc/data.json?resource=9050
+
+
+#start main()
+
+if __name__== "__main__":
+    
+    for country in EU_memberstates:
+        #queryBgprank("9050")
+        #queryTeamCymru("9050")
+        asnList=countryQueryRipe(country)#returns a list of ASN for a country
+        
+        #don't query the ASNs with lastupdate < time-1d
+        
+        dontCheck=db.asDB.find({"country":country , "lastupdate":{"$gt":time.time()-168*3600}})
+        
+        i=0
+        for AS in dontCheck:
+            i=i+1
+            asnList.remove(AS["asn"])
+        print "Removed %i AS with fresh info" % i
+
+        i=0
+        for asnum in asnList:
+            i=i+1
+            print "Querying AS %i / %i" % (i,len(asnList))
+            asnode=Asnode()
+            #Populate the object
+            asnode.asn=asnum
+            asnode.name="AS"+str(asnum)
+            asnode.country=country
+            asnode.peers=peerQueryRipe(asnum)
+            asnode.bgprank=queryBgprank(asnum)
+            asnode.lastupdate=time.time()
+            db.asDB.remove({"asn":asnum})
+            db.asDB.insert(asnode.__dict__)
+            
+    
+    col= db.asDB.find()
+    for doc in col:
+        print doc
+        #peerQueryRipe("9050")
+      
+        
+    
+    
---
+++ graphbuild.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+'''
+Description: A tool to query the mongodb AS database and build the graph with peers and BGPrank of each peer. Out can be to Gephi format.
+Prerequisites: pymongo, json, networkx, matplotlib 
+
+
+'''
+__author__= 'Cosmin Ciobanu, Romain Bourgue, Razvan Gavrila'
+__license__ = 'GPL'
+__contact__ = 'cosmin.ciobanu at gmail dot com'
+__version__ = 1.0
+
+import pymongo,json
+import sys
+import networkx as nx
+import matplotlib.pyplot as plt
+
+#setup MongoDB database connection 
+connect = pymongo.MongoClient("localhost", 27017)
+db = connect.asnum
+
+G=nx.Graph()
+
+#Declare empty node dictionary and empty list of links
+nodes={}
+links=[]
+
+#Query database
+results = db.asDB.find()
+i=0
+for res in results: #iterate through the results fron database
+    i=1+i
+    print "Doing %s of %s asn" % (i,results.count())
+    G.add_node(res["asn"], country = res["country"]) #populate graph nodes
+    j=0
+    for peer in res["peers"]:
+        j=j+1
+        #print "     Doing %s of %s asn" % (j,len(res["peers"]))
+        
+        peer_obj=db.asDB.find_one({"asn":peer},fields=["bgprank"])
+        
+        if(peer_obj!=None):
+            G.add_edge(res["asn"], peer, weight=float(res["bgprank"]) + float(peer_obj["bgprank"]))
+        else:#unstored asn
+            print "No record for asn %s" % peer
+            #G.add_edge(res["asn"], peer, weight=2)   #populate graph edges
+
+nx.write_gexf(G, "EUbadboys.gexf")
+print G.number_of_nodes()
+print G.number_of_edges()
+
+
+
+
+#        nodes[res["asn"]]={"name":res["asn"],"group":res["country"], "rank":res["bgprank"]} #populate the node dictionary    
+#        for peer in res["peers"]:
+#            links.append({'source':res["asn"], "target":peer, 'value':1})
+        
+#print json.dumps({"nodes":nodes,"links":links}) 
+
+
+
+#f=open("ASRomania.json", "w")
+#f.write(json_d3js)
+#f.close
